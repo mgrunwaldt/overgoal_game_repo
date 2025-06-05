df4da495a67a49c832bf3d8dae74a7e6baa0f7b1
Marco Araya Jiménez <75222804+coxmars@users.noreply.github.com>
2025-06-04 19:45:37 -0600

    feat: add deployment files and organization in client (#12)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), wasm(), topLevelAwait()],
+    server: {
+        port: 3001,
+        //    https: {
+        //      key: fs.readFileSync("mkcert+1-key.pem"), // Path to private key file
+        //      cert: fs.readFileSync("mkcert+1.pem"),   // Path to certificate file
+        //  },
+    },
 });
