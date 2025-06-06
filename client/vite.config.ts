73c51508291c0229b2bd49fa7c4ca84aba53c12b
jimenezz22 <ljimenezbogantes@gmail.com>
2025-06-06 00:04:08 -0600

    fix: comment out unused fs import and https configuration in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,16 +2,16 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
-import fs from "fs";
+//import fs from "fs";
 
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait()],
   server: {
     port: 3002,
-    https: {
-      key: fs.readFileSync("mkcert+1-key.pem"),
-      cert: fs.readFileSync("mkcert+1.pem"),
-    },
+    // https: {
+    //   key: fs.readFileSync("mkcert+1-key.pem"),
+    //   cert: fs.readFileSync("mkcert+1.pem"),
+    // },
   },
   define: {
     global: 'globalThis',
